8935177fe59b3e07321e96e5f4698cd3e5651cfc
Henry-Peti <iamgadgetboy@gmail.com>
2022-01-31 12:42:00 +1300

    playing around with tailwindcss

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,12 @@ class Main extends Component {
 
   render() {
     return (
-      <div className="container-fluid text-monospace">
+      <div className="container-fluid text-roboto">
           <br></br>
           &nbsp;
           <br></br>
           <div className="row">
-            <div className="col-md-10">
+            <div className="col-md-9">
               <div className="embed-responsive embed-responsive-16by9" style={{ maxHeight: '768px'}}>
                 <video
                   src={`https://ipfs.infura.io/ipfs/${this.props.currentHash}`}
@@ -19,34 +19,36 @@ class Main extends Component {
               </div>
             <h3><b><i>{this.props.currentTitle}</i></b></h3>
           </div>
-          <div className="col-md-2 border border-danger overflow-auto text-center" style={{ maxHeight: '768px', minWidth: '175px' }}>
-          <h1 className="flex-auto text-sm font-bold text-roboto-900">
-        Share Your Video
-      </h1>
+          <div className="col-md-3 overflow-auto text-center" style={{ maxHeight: '768px', minWidth: '175px' }}>
+          <h1 className="text-xl font-medium text-black">Share Your Video</h1>
             <form onSubmit={(event) => {
               event.preventDefault()
               const title = this.videoTitle.value
               this.props.uploadVideo(title)
             }} >
               &nbsp;
-              <input type='file' accept=".mp4, .mkv, .ogg, .wmv, .mov" onChange={this.props.captureFile} style={{ width: '250px' }} />
-                <div className="form-group mr-sm-2">
+              <input type='file' accept=".mp4, .mkv, .ogg, .wmv, .mov" onChange={this.props.captureFile} style={{ width: '350px' }} />
+                <div className="form-group mr-sm-3">
                   <b></b>
+                  <br></br>
                   <input
                     id="videoTitle"
                     type="text"
                     ref={(input) => { this.videoTitle = input }}
-                    className="form-control-md"
+                    className="form-control-lg"
                     placeholder="Title..."
                     required />
                 </div>
-              <button type="submit" className="hover:bg-black-400 group flex items-center rounded-md btn-danger text-white text-md leading-6 font-medium py-2 px-3 rounded-lg">Upload!</button>
+              <button type="submit" className="py-2 px-5 hover:bg-red-700 group flex justify-center 
+              items-center rounded-md btn-danger text-white text-md leading-6 text-base font-semibold shadow-md 
+              rounded-lg">Upload!</button>
               &nbsp;
               <br></br>
             </form>
+            <br></br>
             { this.props.videos.map((video, key) => {
               return(
-                <div className="card mb-4 text-center bg-secondary mx-auto" style={{ width: '175px'}} key={key} >
+                <div className="card mb-4 text-center bg-secondary mx-auto" style={{ width: 'auto'}} key={key} >
                   <div className="card-title bg-dark">
                     <small className="text-white"><b>{video.title}</b></small>
                   </div>
@@ -54,7 +56,7 @@ class Main extends Component {
                     <p onClick={() => this.props.changeVideo(video.hash, video.title)}>
                       <video
                         src={`https://ipfs.infura.io/ipfs/${video.hash}`}
-                        style={{ width: '150px' }}
+                        style={{ width: '200px' }}
                       />
                     </p>
                   </div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit',
+    purge: ["./src/**/*.{html,js,jsx,ts,tsx}", './public/index.html'],
+    darkMode: 'false',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
